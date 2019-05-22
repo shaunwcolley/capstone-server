ad3e449d98ddc5f1dd2d23d2b0d17f7b1943b61f
Shaun Colley <swc7838@gmail.com>
2019-05-22 13:46:57 -0500

    saving minor files

---
+++ data/resolvers.js
@@ -0,0 +1,41 @@
+const resolvers = {
+  Query: {
+    users: (parent, args, { db }) => db.user.findAll(),
+    stats: (parent, args, { db }) => db.stat.findAll(),
+  },
+  Mutation: {
+    createUser: (parent, { username, password }, { db }) => db.User.create({
+      username,
+      password,
+    }),
+    createStat: (parent, {
+      websiteId,
+      performance,
+      accessibility,
+      bestPractices,
+      seo,
+      timeFetch,
+      timeToFirstByte,
+      firstContentfulPaint,
+      firstMeaningfulPaint,
+      speedIndex,
+      timeToInteractive,
+      estimatedInputLatency,
+    }, { db }) => db.Stat.create({
+      website_id: websiteId,
+      performance,
+      accessibility,
+      best_practices: bestPractices,
+      seo,
+      time_fetch: timeFetch,
+      time_to_first_byte: timeToFirstByte,
+      first_contentful_paint: firstContentfulPaint,
+      first_meaningful_paint: firstMeaningfulPaint,
+      speed_index: speedIndex,
+      time_to_interactive: timeToInteractive,
+      estimated_input_latency: estimatedInputLatency,
+    }),
+  },
+};
+
+module.exports = resolvers;
