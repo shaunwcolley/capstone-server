03977907b8111cfed1c35e1bc18bf6c69407cc1f
Shaun Colley <swc7838@gmail.com>
2019-05-27 13:34:36 -0500

    catching error for website not being secure

--- utils/lighthouseFetch.js
+++ utils/lighthouseFetch.js
@@ -2,22 +2,33 @@ const lighthouse = require('lighthouse');
 const chromeLauncher = require('chrome-launcher');
 const postResult = require('./postLighthouseResult');
 
+
+function checkMethod(config) {
+  let method = '';
+  if (config === 'desktop') {
+    method = 'desktop';
+    return method;
+  }
+  method = 'mobile';
+  return method;
+}
+
 function launchChromeAndRunLighthouse(url, opts, config, websiteId) {
   return chromeLauncher.launch({ chromeFlags: opts.chromeFlags })
     .then((chrome) => {
+      const method = checkMethod(config.settings.emulatedFormFactor);
       opts.port = chrome.port;
       return lighthouse(url, opts, config)
         .then(async (result) => {
           await chrome.kill().then(async () => {
-            let method = '';
-            if (config.settings.emulatedFormFactor === 'desktop') {
-              method = 'desktop';
-            } else {
-              method = 'mobile';
-            }
             await postResult(result, websiteId, method).catch(error => console.log(`Error in posting result to db; error log: ${error}`));
           }).catch(error => console.log(`Error in closing chrome; error log: ${error}`));
-        }).catch(error => console.log(`Error in running lighthouse api; error log: ${error}`));
+        }).catch(async (error) => {
+          await chrome.kill().then(async () => {
+            await postResult(error, websiteId, method).catch(postError => console.log(`Error in posting result to db; error log: ${postError}`));
+            console.log(`Error in running lighthouse api; error log: ${error}`);
+          }).catch(chromeError => console.log(`Error in closing chrome; error log: ${chromeError}`));
+        });
     });
 }
 
--- utils/postLighthouseResult.js
+++ utils/postLighthouseResult.js
@@ -1,43 +1,68 @@
 const db = require('../models');
 
 const postLighthouseResult = async (result, websiteId, method) => {
-  const {
-    lhr:
-    {
-      categories: {
-        performance: { score: performance },
-        accessibility: { score: accessibility },
-        'best-practices': { score: bestPractices },
-        seo: { score: seo },
+  if (result.lhr) {
+    const {
+      lhr:
+      {
+        categories: {
+          performance: { score: performance },
+          accessibility: { score: accessibility },
+          'best-practices': { score: bestPractices },
+          seo: { score: seo },
+        },
+        fetchTime,
+        audits: {
+          'first-contentful-paint': { displayValue: firstContentfulPaint },
+          'first-meaningful-paint': { displayValue: firstMeaningfulPaint },
+          'time-to-first-byte': { displayValue: timeToFirstByte },
+          'speed-index': { displayValue: speedIndex },
+          interactive: { displayValue: timeToInteractive },
+          'estimated-input-latency': { displayValue: estimatedInputLatency },
+        },
       },
-      fetchTime,
-      audits: {
-        'first-contentful-paint': { displayValue: firstContentfulPaint },
-        'first-meaningful-paint': { displayValue: firstMeaningfulPaint },
-        'time-to-first-byte': { displayValue: timeToFirstByte },
-        'speed-index': { displayValue: speedIndex },
-        interactive: { displayValue: timeToInteractive },
-        'estimated-input-latency': { displayValue: estimatedInputLatency },
-      },
-    },
-  } = result;
+    } = result;
 
-  await db.Stat.create({
-    method,
-    website_id: websiteId,
-    performance,
-    accessibility,
-    best_practices: bestPractices,
-    seo,
-    time_fetch: fetchTime,
-    time_to_first_byte: timeToFirstByte,
-    first_contentful_paint: firstContentfulPaint,
-    first_meaningful_paint: firstMeaningfulPaint,
-    speed_index: speedIndex,
-    time_to_interactive: timeToInteractive,
-    estimated_input_latency: estimatedInputLatency,
-  });
+    await db.Stat.create({
+      method,
+      website_id: websiteId,
+      performance,
+      accessibility,
+      best_practices: bestPractices,
+      seo,
+      time_fetch: fetchTime,
+      time_to_first_byte: timeToFirstByte,
+      first_contentful_paint: firstContentfulPaint,
+      first_meaningful_paint: firstMeaningfulPaint,
+      speed_index: speedIndex,
+      time_to_interactive: timeToInteractive,
+      estimated_input_latency: estimatedInputLatency,
+    });
+  } else {
+    const {
+      LHError: errorMessage,
+    } = result;
+
+    await db.Stat.create({
+      method,
+      website_id: websiteId,
+      performance: null,
+      accessibility: null,
+      best_practices: null,
+      seo: null,
+      time_fetch: null,
+      time_to_first_byte: null,
+      first_contentful_paint: null,
+      first_meaningful_paint: null,
+      speed_index: null,
+      time_to_interactive: null,
+      estimated_input_latency: null,
+      error_code: 'LHError',
+      error_message: errorMessage,
+    });
+  }
 };
+
 
 module.exports = postLighthouseResult;
 
