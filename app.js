122dda4ab8c903307c3dfe2465e3166980c30b4e
Shaun Colley <swc7838@gmail.com>
2019-05-28 12:36:08 -0500

    changes to test config

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const typeDefs = require('./data/schema');
 const resolvers = require('./data/resolvers');
 const desktopConfig = require('./utils/lr-desktop-config');
 const mobileConfig = require('./utils/lr-mobile-config');
+const baseConfig = require('./utils/baseConfig');
 
 
 const server = new ApolloServer({
@@ -23,19 +24,28 @@ async function processWebsites(array) {
  for(const item of array) {
    const { id, url } = item;
     await launchChromeAndRunLighthouse(url, opts, desktopConfig, id);
+    // mobile is only off in accessibility by 15, rest by 1%
     await launchChromeAndRunLighthouse(url, opts, mobileConfig, id);
+    // More accurate in the following: paints, speed index, performance, bestPractices and seo,
+    // but still not as accurate as it should be for seo (BestPractices were off by 7 instead of 14)
+    // Seo off by 19,
+    await launchChromeAndRunLighthouse(url, opts, baseConfig, id);
  }
 }
 
-const job = schedule.scheduleJob({ hour: 8, minute: 30, dayOfWeek: 2 }, async () => {
-  db.Website.findAll()
-    .then((websites) => {
-      processWebsites(websites).catch(error => console.log(error));
-    });
-  console.log('LH process was fired, logging websites to db.');
-});
+// const job = schedule.scheduleJob({ hour: 8, minute: 30, dayOfWeek: 2 }, async () => {
+//   db.Website.findAll()
+//     .then((websites) => {
+//       processWebsites(websites).catch(error => console.log(error));
+//     });
+//   console.log('LH process was fired, logging websites to db.');
+// });
+//
+// job.schedule();
 
-job.schedule();
+const testWebsites = [{ id: 1, url: 'https://www.google.com' }];
+
+// processWebsites(testWebsites).catch(error => console.log(error));
 
 
 const PORT = process.env.PORT || 8080;
---
+++ utils/baseConfig.js
@@ -0,0 +1,5 @@
+const config = {
+  extends: 'lighthouse:default',
+};
+
+module.exports = config;
