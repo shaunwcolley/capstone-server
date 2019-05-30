5d34d7a50fb7c29dd78519d53548f29a448c1cb4
Shaun Colley <swc7838@gmail.com>
2019-05-29 23:45:49 -0500

    authentication finished

--- data/resolvers.js
+++ data/resolvers.js
@@ -3,23 +3,44 @@ const login = require('../utils/auth/login');
 
 const resolvers = {
   Query: {
-    users: (parent, args, { db }) => db.User.findAll(),
-    stats: (parent, args, { db }) => db.Stat.findAll({ include: [{ model: db.Website, as: 'website' }] }),
-    websites: (parent, args, { db }) => db.Website.findAll(),
-    sortWebsite: (parent, args, { db }) => db.Stat.findAll({ include: [{ model: db.Website, as: 'website' }] }),
+    users: (parent, args, { db, user }) => {
+      if (!user) return null;
+      return db.User.findAll();
+    },
+    stats: (parent, args, { db, user }) => {
+      if (!user) return null;
+      return db.Stat.findAll({ include: [{ model: db.Website, as: 'website' }] });
+    },
+    websites: (parent, args, { db, user }) => {
+      if (!user) return null;
+      return db.Website.findAll();
+    },
+    sortWebsite: (parent, args, { db, user }) => {
+      if (!user) return null;
+      return db.Stat.findAll({ include: [{ model: db.Website, as: 'website' }] });
+    },
   },
   Mutation: {
-    createUser: (parent, { username, password }, { db }) => db.User.create({
-      username,
-      password,
-    }),
-    createWebsite: (parent, { name, url }, { db }) => db.Website.create({
-      name,
-      url,
-    }),
-    runLighthouse: (parent, args, { db }) => db.Website.findAll().then((websites) => {
-      processWebsites(websites).catch(error => console.log(error));
-    }),
+    createUser: (parent, { username, password }, { db, user }) => {
+      if (!user) return null;
+      return db.User.create({
+        username,
+        password,
+      });
+    },
+    createWebsite: (parent, { name, url }, { db, user }) => {
+      if (!user) return null;
+      return db.Website.create({
+        name,
+        url,
+      });
+    },
+    runLighthouse: (parent, args, { db, user }) => {
+      if (!user) return null;
+      return db.Website.findAll().then((websites) => {
+        processWebsites(websites).catch(error => console.log(error));
+      });
+    },
     login: (parent, { username, password }, { db }) => db.User.findAll({ where: { username } })
       .then(users => login(users, password)),
   },
